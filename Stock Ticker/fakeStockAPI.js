b0c713ad42a48f322dcafd035e822c3aeb03587e
vFizor <jana.valdezcw@gmail.com>
2026-02-24 03:40:32 -0500

    /n

---
+++ Stock Ticker/fakeStockAPI.js
@@ -0,0 +1,23 @@
+
+function getStockData () {
+    getStockData.time = Math.random().toFixed(2)
+    return {
+        name: 'QtechAI',
+        sym: 'QTA',
+        price: '',
+        time: ''
+    }
+}
+
+let stockData = getStockData()
+
+stockData.price = Math.random().toFixed(2)
+
+
+stockData.time = new Date().toLocaleTimeString('en-GB', {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+});
+
+console.log(stockData)
---
+++ Stock Ticker/index.js
@@ -0,0 +1,9 @@
+
+
+function renderStockTicker (stockData) {
+    const stockDisplayName = document.getElementById('name')
+    const stockDisplaySymbol = document.getElementById('symbol')
+    const stockDisplayPrice = document.getElementById ('price')
+    const stockDisplayPriceIcon = document.getElementById ('price-icon')
+    const stockDisplayTime = document.getElementById('time')
+}
